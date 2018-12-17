a45d7fad5a6b4c31aa36113cf8208d45e96630f0
Oisin Redmond <oisinredmond@gmail.com>
2018-12-17 19:44:27 +0000

    Updated scripts and assets

--- Assets/Scripts/SpiralVisualiser.cs
+++ Assets/Scripts/SpiralVisualiser.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 
 public class SpiralVisualiser : MonoBehaviour {
 
+    public GameObject spiralPrefab;
+    public float scale = 10;
+    private Vector3 pos;
     List<GameObject> spirals1 = new List<GameObject>();
 	// Use this for initialization
 	void Start () {
-		
+        GameObject spiralObject = Instantiate(spiralPrefab, new Vector3(0,0,0), Quaternion.identity);
+        spiralObject.transform.parent = this.transform;
+        Spiral spiralScript = spiralObject.GetComponent<Spiral>();
+        spiralScript.theta = 137.5f;
+        spiralScript.scale = 1;
+        spiralScript.steps = 1;
+        spiralScript.max = 1000;
+        spiralScript.lerpInterval = 0.1f;
+        //Spirals1();
 	}
 	
 	// Update is called once per frame
