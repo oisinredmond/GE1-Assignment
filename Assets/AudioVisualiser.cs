b68fb177ff9028c0eb60892000a9a1871289ae46
Oisin Redmond <oisinredmond@gmail.com>
2018-12-17 16:54:54 +0000

    Added public fields

--- Assets/AudioVisualiser.cs
+++ Assets/AudioVisualiser.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AudioVisualiser : MonoBehaviour {
-
+    public float scale = 20;
+    public float radius = 15;
+    List<GameObject> elements = new List<GameObject>();
 	// Use this for initialization
 	void Start () {
 		
