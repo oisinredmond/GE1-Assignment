a4531bc224c0e861284dc1bb77cc477df4126b74
Oisin Redmond <oisinredmond@gmail.com>
2018-12-17 16:29:08 +0000

    Updated AudioAnalyser method

--- Assets/AudioAnalyser.cs
+++ Assets/AudioAnalyser.cs
@@ -9,15 +9,22 @@ public class AudioAnalyser : MonoBehaviour {
 
     public bool useMic = false;
     public AudioClip audioClip;
-    AudioSource aSource;
+    AudioSource audioSource;
+    public AudioMixerGroup master;
 
-    public static int framSize = 512;
+    public static int frameSize = 512;
     public static float[] spectrum;
     public static float[] bands;
+    public float binWidth;
+    public float sampleRate;
 
     public void Awake()
     {
-        
+        audioSource = GetComponent<AudioSource>();
+        spectrum = new float[frameSize];
+        bands = new float[(int)Mathf.Log(frameSize, 2)];
+        audioSource.clip = audioClip;
+        audioSource.outputAudioMixerGroup = master;
     }
 
     // Use this for initialization
