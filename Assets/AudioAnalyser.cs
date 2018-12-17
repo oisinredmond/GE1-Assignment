7922721ab5bbd63aa4377577acbbf97fd3e2077f
Oisin Redmond <oisinredmond@gmail.com>
2018-12-17 16:17:14 +0000

    updated scripts

---
+++ Assets/AudioAnalyser.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+[RequireComponent(typeof(AudioSource))]
+
+public class AudioAnalyser : MonoBehaviour {
+
+    public bool useMic = false;
+    public AudioClip audioClip;
+    AudioSource aSource;
+
+    public static int framSize = 512;
+    public static float[] spectrum;
+    public static float[] bands;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
