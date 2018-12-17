f2f526ff2902f374411e3c8bdc18389850a03feb
Oisin Redmond <oisinredmond@gmail.com>
2018-12-17 17:48:56 +0000

    Added method to calculate phyllotaxis position

--- Assets/Scripts/Phyllotaxis.cs
+++ Assets/Scripts/Phyllotaxis.cs
@@ -4,13 +4,27 @@ using UnityEngine;
 
 public class Phyllotaxis : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    public GameObject dot;
+    public float theta, scale;
+    public int n;
+    public float dotScale;
+
+    private Vector2 calcPT(float calcTheta, float calcScale, int count)
+    {
+        float angle = count * (calcTheta * Mathf.Deg2Rad);
+        float r = scale * calcScale * Mathf.Sqrt(count);
+        float x = r * (float)Mathf.Cos(angle);
+        float y = r * (float)Mathf.Sin(angle);
+        Vector2 pos = new Vector2(x, y);
+        return pos;
+    }
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update () {		
 	}
 }
