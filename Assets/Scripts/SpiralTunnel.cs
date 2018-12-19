5adce676707558284e5a65726dd9bea32f515042
Oisin Redmond <oisinredmond@gmail.com>
2018-12-19 18:08:11 +0000

    Fixed positioning issues

--- Assets/Scripts/SpiralTunnel.cs
+++ Assets/Scripts/SpiralTunnel.cs
@@ -3,9 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SpiralTunnel : MonoBehaviour {
-    public Transform tunnel;
-    public float speed, dist;
-
+    
 	// Use this for initialization
 	void Start () {
 		
@@ -13,7 +11,6 @@ public class SpiralTunnel : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        tunnel.position = new Vector3(tunnel.position.x, tunnel.position.y, tunnel.position.z);
-        this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, tunnel.position.z + dist);
+
 	}
 }
